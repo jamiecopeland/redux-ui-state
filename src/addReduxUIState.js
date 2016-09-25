76b52d7aff6957c7e1c44c52989537a884db7469
Jamie Copeland <jamie@jamiecopeland.com>
2016-09-25 20:16:27 +0100

    Refactor addReduxUIState down to single wrapper component

--- src/addReduxUIState.js
+++ src/addReduxUIState.js
@@ -2,89 +2,70 @@ import React from 'react';
 
 import { setUIState, replaceUIState } from './actions';
 
+const mapStateToProps = (state, id) => ({
+  uiState: state.components[id],
+});
+
+const mapDispatchToProps = (dispatch, props, id, getInitialState) => ({
+  setUIState: (newState, shouldDeepMerge) => dispatch(setUIState({
+    id,
+    shouldDeepMerge,
+    state: newState,
+  })),
+  replaceUIState: newState => dispatch(replaceUIState({
+    id,
+    state: newState,
+  })),
+  resetUIState: () => dispatch(replaceUIState({
+    id,
+    state: getInitialState(props),
+  })),
+});
+
 const addReduxUIState = (
   { id, getInitialState, destroyOnUnmount }
-) => WrappedComponent => {
-  function mapStateToProps(state) {
-    return {
-      uiState: state.components[id],
-    };
-  }
-
-  function mapDispatchToProps(dispatch, props) {
-    return {
-      setUIState: (newState, shouldDeepMerge) => {
-        dispatch(setUIState({
-          id,
-          shouldDeepMerge,
-          state: newState,
-        }));
-      },
-      replaceUIState: (newState) => {
-        dispatch(replaceUIState({
-          id,
-          state: newState,
-        }));
-      },
-      resetUIState: () => {
-        dispatch(replaceUIState({
-          id,
-          state: getInitialState(props),
-        }));
-      },
-    };
-  }
-
-  class ExportedComponent extends React.Component {
-    static propTypes = {
-      uiState: React.PropTypes.object,
-      setUIState: React.PropTypes.func,
-      replaceUIState: React.PropTypes.func,
-      resetUIState: React.PropTypes.func,
-    };
-
-    componentDidMount() {
-      this.props.setUIState(getInitialState(this.props));
-    }
-
-    componentWillUnmount() {
-      if (destroyOnUnmount) {
-        this.props.resetUIState();
-      }
-    }
-
-    render() {
-      return (
-        this.props.uiState
-        ? <WrappedComponent {...this.props} />
-        : null
-      );
-    }
-  }
-
-  const ProxyComponent = (props) => {
-    if (!props.uiStateBranch || !props.dispatch) {
-      throw new Error(
-        'Cannot find state and dispatch in props. This probably means you\'re using ' +
-        'addReduxUIState with useConnect set to false but havent passed state and dispatch ' +
-        'in your component\'s mapStateToProps and mapDispatchToProps functions.'
-      );
-    }
-    return (
-      <ExportedComponent {...{
-        ...mapStateToProps(props.uiStateBranch),
-        ...mapDispatchToProps(props.dispatch),
-      }}
-      />
-    );
-  };
-  ProxyComponent.propTypes = {
+) => WrappedComponent => class ExportedComponent extends React.Component {
+  static propTypes = {
     uiStateBranch: React.PropTypes.object.isRequired,
     dispatch: React.PropTypes.func.isRequired,
-  };
+  }
 
-  return ProxyComponent;
+  constructor(props) {
+    super(props);
+    if (!props.uiStateBranch || !props.dispatch) {
+      throw new Error(
+        'Cannot find uiStateBranch and dispatch in props. This probably means you\'re using ' +
+        'addReduxUIState rather than addReduxUISTateWithConnect and havent passed the required ' +
+        'props through in your component\'s mapStateToProps and mapDispatchToProps functions.'
+      );
+    }
+    this.mappedDispatchProps = mapDispatchToProps(this.props.dispatch, props, id, getInitialState);
+  }
+
+  componentDidMount() {
+    this.mappedDispatchProps.setUIState(getInitialState(this.props));
+  }
+
+  componentWillUnmount() {
+    if (destroyOnUnmount) {
+      this.mappedDispatchProps.resetUIState();
+    }
+  }
+
+  render() {
+    const mappedStateProps = mapStateToProps(this.props.uiStateBranch, id);
+    return (
+      mappedStateProps.uiState
+      ? (
+        <WrappedComponent {...{
+          ...mappedStateProps,
+          ...this.mappedDispatchProps,
+        }}
+        />
+      )
+      : null
+    );
+  }
 };
 
 export default addReduxUIState;
-
