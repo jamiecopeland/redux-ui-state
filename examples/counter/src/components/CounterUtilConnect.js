989e16b43e704c4dc2833d86df93444fcff86a82
Jamie Copeland <jamie@jamiecopeland.com>
2016-09-25 23:25:58 +0100

    Add new basic counter example

---
+++ examples/counter/src/components/CounterUtilConnect.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { compose } from 'redux';
+
+import { addReduxUIState, createConnectWrapper } from 'redux-ui-state';
+
+const Counter = ({ uiState, setUIState }) => (
+  <div>
+    <div>
+      {uiState.index}
+    </div>
+    <div>
+      <button onClick={() => setUIState({ index: uiState.index - 1 })}>-</button>
+      <button onClick={() => setUIState({ index: uiState.index + 1 })}>+</button>
+    </div>
+  </div>
+);
+
+Counter.propTypes = {
+  uiState: React.PropTypes.object.isRequired,
+  setUIState: React.PropTypes.func.isRequired,
+};
+
+const config = {
+  id: 'counterUtil',
+  getInitialState: () => ({ index: 0 }),
+};
+
+export default compose(
+  createConnectWrapper(),
+  addReduxUIState(config)
+)(Counter);
---
+++ examples/counter/src/reducers/index.js
@@ -0,0 +1,5 @@
+import { reduxUIStateReducer, UI_STATE_BRANCH_NAME } from 'redux-ui-state';
+
+export default (state = {}, action) => ({
+  [UI_STATE_BRANCH_NAME]: reduxUIStateReducer(state[UI_STATE_BRANCH_NAME], action),
+});
