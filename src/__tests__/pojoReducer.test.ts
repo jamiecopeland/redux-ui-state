156619721826a46b132b034252b6effc5f9acac3
Jamie Copeland <Jamie.Copeland@thetrainline.com>
2017-01-11 15:38:59 +0000

    Switch previous tests over to Jest

--- src/__tests__/pojoReducer.test.ts
+++ src/__tests__/pojoReducer.test.ts
@@ -1,5 +1,117 @@
+import { reducer, initialState as defaultInitialState } from '../pojoReducer';
+import { setUIState, replaceUIState } from '../actions';
+import { UIStateBranch } from '../addReduxUIState';
+
+const COMPONENT_ID = 'thing';
+
+interface ComponentUIState {
+  readonly firstName?: string;
+  readonly lastName?: string;
+}
+
+const getUser = (): ComponentUIState => ({
+  firstName: 'Darth',
+  lastName: 'Vader',
+});
+
 describe('pojoReducer', () => {
-  it('should do something', () => {
-    expect(true).toEqual(true);
+
+  describe('SET_UI_STATE', () => {
+    it('set a new primitive value in empty initial state', () => {
+      const action = setUIState<ComponentUIState>({
+        state: {
+          firstName: getUser().firstName,
+        },
+        id: COMPONENT_ID,
+      });
+      const newState = reducer(defaultInitialState, action);
+
+      expect(newState).toEqual({
+        components: {
+          [COMPONENT_ID]: {
+            firstName: getUser().firstName,
+          },
+        },
+      });
+    });
+
+    it('set a new primitive value in populated initial state', () => {
+      const initialState: UIStateBranch = {
+        components: {
+          [COMPONENT_ID]: {
+            lastName: getUser().lastName,
+          },
+        },
+      };
+      const action = setUIState<ComponentUIState>({
+        state: {
+          firstName: getUser().firstName,
+        },
+        id: COMPONENT_ID,
+      });
+      const newState = reducer(initialState, action);
+      expect(newState).toEqual({
+        components: {
+          [COMPONENT_ID]: {
+            firstName: getUser().firstName,
+            lastName: getUser().lastName,
+          },
+        },
+      });
+    });
+  });
+
+  describe('REPLACE_UI_STATE', () => {
+    it('should replace ui state with empty state', () => {
+      const initialState: UIStateBranch = {
+        components: {
+          [COMPONENT_ID]: {
+            firstName: getUser().firstName,
+          },
+        },
+      };
+
+      const replacementState: ComponentUIState = {
+        lastName: getUser().lastName,
+      };
+
+      const action = replaceUIState({
+        id: COMPONENT_ID,
+        state: replacementState,
+      });
+
+      const newState = reducer(initialState, action);
+      expect(newState).toEqual({
+        components: {
+          [COMPONENT_ID]: replacementState,
+        },
+      });
+    });
+
+    it('should replace ui state with populated state', () => {
+      const initialState: UIStateBranch = {
+        components: {
+          [COMPONENT_ID]: {
+            firstName: getUser().firstName,
+          },
+        },
+      };
+
+      const replacementState: ComponentUIState = {
+        lastName: getUser().lastName,
+      };
+
+      const action = replaceUIState<ComponentUIState>({
+        id: COMPONENT_ID,
+        state: replacementState,
+      });
+
+      const newState = reducer(initialState, action);
+      expect(newState).toEqual({
+        components: {
+          [COMPONENT_ID]: replacementState,
+        },
+      });
+    });
   });
 });
