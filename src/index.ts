6c4a99732dbe67316348be6b5c13b8ced3c6f214
Jamie Copeland <Jamie.Copeland@thetrainline.com>
2017-07-02 22:10:39 +0100

    Add all util exports to export aggregator

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,39 @@
 export {
-  DEFAULT_BRANCH_NAME,
+  IdFunction,
+  Id,
+  idIsString,
+  idIsFunction,
+  getStringFromId,
+  DefaultStoreState,
+  UIStateBranch,
   StateProps,
   DispatchProps,
   Props,
-  UIStateBranch,
-  DefaultStoreState,
-  Id,
+  AbstractWrappedComponent,
+  WrappedComponentWithoutTransform,
+  WrappedComponentWithTransform,
+  UIStateBranchSelector,
+  UIStateIdProps,
+  SetUIStateFunction,
   TransformPropsFunction,
+  DEFAULT_BRANCH_NAME,
+  stateSelector,
+  propsSelector,
   defaultUIStateBranchSelector,
+  UIStateBranchSelectorSelectorProps,
+  uiStateBranchSelectorSelector,
+  uiStateBranchSelector,
+  idSelector,
   uiStateSelector,
   setUIStateSelector
 } from './utils';
 
 export {
+  ModifyUIStateActionPayload,
+  ModifyUIStateAction,
   SET_UI_STATE,
   setUIState,
 } from './actions';
 
 export { createReducer } from './reducer';
-export { defaultConnectUIState, createConnectUIState } from './connectUIState';
+export { createConnectUIState, defaultConnectUIState } from './connectUIState';
